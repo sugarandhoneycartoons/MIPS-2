115a628cb6c32dca781fc4defb57adfdc6080528
sugarandhoneycartoons <60756328+sugarandhoneycartoons@users.noreply.github.com>
2020-10-31 14:10:33 -0400

    Update project2.s
    It's creating a variable for the total. It's also loading a string to an address

--- project2.s
+++ project2.s
@@ -11,3 +11,6 @@
     	la $a1, string
     	li $v0, 8
     	syscall
+
+		move $t3, $zero #this will be the "total" variable and it will have a value of zero
+		la $t0, string #loads the string to this address
